fe2f5be30348a8f43aded4b05809462500d0a397
SiFaisal <sifaisalcox@gmail.com>
2024-01-28 23:42:54 +0600

    resolve fast userload

--- src/Components/RegisterUser/RegisterUser.jsx
+++ src/Components/RegisterUser/RegisterUser.jsx
@@ -7,14 +7,14 @@ const RegisterUser = () => {
    
     return (
         <div className='bg-slate-300 p-2 sticky top-0'>
-            <h1 className='text-3xl text-green-600'>All Register User</h1>
+            <h1 className='text-2xl text-green-600'>All Register User</h1>
             <ul>
                 {
                     usersDB?.map((user, i) =>{
-                       return ( <li key={user._id} className='flex flex-col my-2'>
+                       return ( <li key={user._id} className='flex flex-col p-2  border-zinc-950 border my-2'>
                            
-                       <span className='text-xl h-auto w-full'>{i+1}. Name:  {user?.name}  </span>
-                       <span className='text-xl h-auto w-full'>Email: {user?.email}  </span>
+                       <span className='text-lg h-auto w-full'>{i+1}. Name:  {user?.name}  </span>
+                       <span className='text-sm text-primary h-auto w-full'>Email: {user?.email}  </span>
                    </li>)
                     })
                 }
